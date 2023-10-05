886fbaa6d02933ea5765b47f1c86a4e1696a9c72
Jose Camarena <josecamarenag01@gmail.com>
2023-10-05 08:56:29 +0200

    .

---
+++ ActSprint1/app/src/main/java/com/example/actsprint1/MessagesWindow.kt
@@ -0,0 +1,11 @@
+package com.example.actsprint1
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MessagesWindow : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_messages_window)
+    }
+}
